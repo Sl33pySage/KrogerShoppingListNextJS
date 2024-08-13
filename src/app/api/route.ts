7a9c6b3eade100da60f913db38a6b39aa35dd6af
Sl33pySage <jasonwolf82@hotmail.com>
2024-08-13 14:54:27 -0500

    HECC NEXTJS

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,25 +1,25 @@
-"use server";
-import TokenHandler from "./token/route";
-import { NextResponse } from "next/server";
+// "use server";
+// import TokenHandler from "./token/route";
+// import { NextResponse } from "next/server";
 
-export default async function ProductListHandler(product: any) {
-  const dataFromTokenHandler = await TokenHandler();
+// export default async function ProductListHandler(product: any) {
+//   const dataFromTokenHandler = await TokenHandler();
 
-  const locationId = process.env.REACT_kroger_store_locationId;
+//   const locationId = process.env.REACT_kroger_store_locationId;
 
-  const productUrl = `https://api-ce.kroger.com/v1/products`;
+//   const productUrl = `https://api-ce.kroger.com/v1/products`;
 
-  const res = await fetch(
-    `${productUrl}?filter.locationId=${locationId}&filter.term=${product}&filter.brand=Kroger`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${await dataFromTokenHandler.access_token}`,
-      },
-    }
-  );
+//   const res = await fetch(
+//     `${productUrl}?filter.locationId=${locationId}&filter.term=${product}&filter.brand=Kroger`,
+//     {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//         Authorization: `Bearer ${await dataFromTokenHandler.access_token}`,
+//       },
+//     }
+//   );
 
-  const data = await res.json();
-  return await data.data;
-}
+//   const data = await res.json();
+//   return await data.data;
+// }
